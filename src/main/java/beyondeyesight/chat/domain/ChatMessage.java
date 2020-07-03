85e7a7040eac9dac1deab06075c4f1abf0ffa49e
junk-sound <wom2277@naver.com>
2020-07-04 00:37:05 +0900

    feat: Redis pub/sub

--- src/main/java/beyondeyesight/chat/domain/ChatMessage.java
+++ src/main/java/beyondeyesight/chat/domain/ChatMessage.java
@@ -3,13 +3,23 @@ package beyondeyesight.chat.domain;
 public class ChatMessage {
     private final ChatRoom chatRoom;
     private final Sender sender;
+    private final String body;
 
-    private ChatMessage(ChatRoom chatRoom, Sender sender) {
+    public ChatMessage(ChatRoom chatRoom, Sender sender, String body) {
         this.chatRoom = chatRoom;
         this.sender = sender;
+        this.body = body;
     }
 
     static ChatMessage of(ChatRoom chatRoom, Sender sender) {
-        return new ChatMessage(chatRoom, sender);
+        return new ChatMessage(chatRoom, sender, "body");
+    }
+
+    public ChatRoom getChatRoom() {
+        return chatRoom;
+    }
+
+    public Sender getSender() {
+        return sender;
     }
 }
