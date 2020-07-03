07b8a8637dc5fbd166c1f5bc53c604eecfa99e6d
junk-sound <wom2277@naver.com>
2020-07-03 22:31:23 +0900

    feat: construct ChatMessage

---
+++ src/test/java/beyondeyesight/chat/domain/ChatMessageTest.java
@@ -0,0 +1,18 @@
+package beyondeyesight.chat.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class ChatMessageTest {
+
+    @Test
+    void of() {
+        ChatRoom mockChatRoom = mock(ChatRoom.class);
+        Sender mockSender = mock(Sender.class);
+        ChatMessage chatMessage = ChatMessage.of(mockChatRoom, mockSender);
+        assertThat(chatMessage).isNotNull();
+    }
+}
---
+++ src/test/java/beyondeyesight/chat/domain/IdTest.java
@@ -0,0 +1,18 @@
+package beyondeyesight.chat.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdTest {
+
+    @DisplayName("#of() : should return new Id")
+    @Test
+    void of() {
+        String testId = "testId";
+        Id id = Id.of(testId);
+        assertThat(id).isNotNull();
+    }
+}
