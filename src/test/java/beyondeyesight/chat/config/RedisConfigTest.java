4f9643d0a2220d9c919760b0ec0c8c58578ee966
junk-sound <wom2277@naver.com>
2020-07-03 23:57:42 +0900

    feat: RedisConfig

---
+++ src/test/java/beyondeyesight/chat/config/RedisConfigTest.java
@@ -0,0 +1,33 @@
+package beyondeyesight.chat.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {RedisConfig.class, LettuceConnectionFactory.class})
+class RedisConfigTest {
+
+    @Autowired
+    private RedisMessageListenerContainer redisMessageListenerContainer;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Test
+    void redisMessageListener() {
+        assertThat(redisMessageListenerContainer).isNotNull();
+    }
+
+    @Test
+    void redisTemplate() {
+        assertThat(redisTemplate).isNotNull();
+    }
+}
