dab7c984caffbabdad04d92c16a749c8b043d8e9
junk-sound <wom2277@naver.com>
2020-07-03 23:57:42 +0900

    feat: RedisConfig

--- src/test/java/beyondeyesight/chat/config/EmbeddedRedisConfigTest.java
+++ src/test/java/beyondeyesight/chat/config/EmbeddedRedisConfigTest.java
@@ -6,10 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import redis.embedded.RedisServer;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @RunWith(SpringRunner.class)
