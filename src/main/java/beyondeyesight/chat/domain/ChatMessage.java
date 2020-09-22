e7d17159944e43cbc492e85b03b22b4335f57096
junk-sound <wom2277@naver.com>
2020-09-22 10:46:33 +0900

    chore: ChattMessage 도메인 고민 주석

--- src/main/java/beyondeyesight/chat/domain/ChatMessage.java
+++ src/main/java/beyondeyesight/chat/domain/ChatMessage.java
@@ -2,6 +2,8 @@ package beyondeyesight.chat.domain;
 
 //todo: check if serializable is needed
 public class ChatMessage {
+    //todo: 얘도 id가 있어야 함.
+    //todo: ChatRoom 자체로 할지, ChatRoomId로 할지 고민.
     private final ChatRoom chatRoom;
     private final Sender sender;
     private final String body;
