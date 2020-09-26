52e578fd8ae62489459ae8ade86b2fbd07fbbeca
junk-sound <wom2277@naver.com>
2020-09-26 11:17:59 +0900

    refac: TestCassandraConfig 로드 시 yml 파일에서 값 가져오기

---
+++ src/test/java/beyondeyesight/chat/config/TestCassandraConfig.java
@@ -0,0 +1,57 @@
+package beyondeyesight.chat.config;
+
+import java.util.Collections;
+import javax.annotation.Nonnull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.lang.NonNull;
+
+@TestConfiguration
+public class TestCassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.data.cassandra.keyspace-name}")
+    private String keyspaceName;
+
+    @Value("${spring.data.cassandra.port}")
+    private int port;
+
+    @Value("${spring.data.cassandra.contact-points}")
+    private String contactPoints;
+
+    @Value("${spring.data.cassandra.local-datacenter}")
+    private String localDatacenter;
+
+    @Override
+    @Bean("testCassandraSession")
+    @Primary
+    @Nonnull
+    public CqlSessionFactoryBean cassandraSession() { ;
+        CqlSessionFactoryBean cqlSessionFactoryBean = new CqlSessionFactoryBean();
+        cqlSessionFactoryBean.setContactPoints(contactPoints);
+        cqlSessionFactoryBean.setPort(port);
+        cqlSessionFactoryBean.setKeyspaceName(keyspaceName);
+        //todo: datacenter 이름 지정해주기
+        cqlSessionFactoryBean.setLocalDatacenter(localDatacenter);
+
+        //todo: 걍 CreateKeyspaceSpecification.createKeyspace 만 해도 될듯?
+        cqlSessionFactoryBean.setKeyspaceCreations(
+            Collections.singletonList(
+                CreateKeyspaceSpecification.createKeyspace(keyspaceName)
+                    .ifNotExists()
+                    .withSimpleReplication(1)
+            )
+        );
+        return cqlSessionFactoryBean;
+    }
+
+    @NonNull
+    @Override
+    protected String getKeyspaceName() {
+        return keyspaceName;
+    }
+}
--- src/test/java/beyondeyesight/chat/infra/persistence/ChatMessageRepositoryTest.java
+++ src/test/java/beyondeyesight/chat/infra/persistence/ChatMessageRepositoryTest.java
@@ -1,13 +1,18 @@
 package beyondeyesight.chat.infra.persistence;
 
+import beyondeyesight.chat.config.TestCassandraConfig;
 import beyondeyesight.chat.domain.ChatMessage;
 import beyondeyesight.chat.domain.ChatRoom;
 import beyondeyesight.chat.domain.Sender;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest()
+@ActiveProfiles("test")
+@ContextConfiguration(classes = TestCassandraConfig.class)
+@SpringBootTest(classes = TestCassandraConfig.class)
 class ChatMessageRepositoryTest {
     @Autowired
     private ChatMessageRepository chatMessageRepository;
@@ -16,7 +21,7 @@ class ChatMessageRepositoryTest {
     void haha() {
         ChatRoom chatRoom = ChatRoom.of("chatRoom");
         Sender sender = Sender.of("sender");
-        ChatMessage chatMessage = ChatMessage.of("chatRoomkk", "sendertt","bodytt");
+        ChatMessage chatMessage = ChatMessage.of("haha", "haha","haha");
 
         chatMessage = chatMessageRepository.save(chatMessage);
         System.out.println(chatMessage);
