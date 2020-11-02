ca5efe84b9b3f14aaf194b9b9e886d364cdc9cef
junk-sound <wom2277@naver.com>
2020-11-02 17:16:38 +0900

    feat: ChatController - Chrome의 Websocket Test Client로 통신 확인

--- src/main/java/beyondeyesight/chat/config/WebSocketConfig.java
+++ src/main/java/beyondeyesight/chat/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -17,7 +18,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
-                .withSockJS();
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.addDecoratorFactory(
+            EmaWebSocketHandlerDecorator::new);
     }
 }
