b3a8cc066a4a3b73e0439b908f66444f9089f47b
Geunwon Lim <wom2277@naver.com>
2020-11-19 15:00:19 +0900

    Feat: git action을 트리거하는 브랜치 변경 및 withSockjs (#124)
    * feat: withSockJS
    
    * refac: ci on

--- src/main/java/beyondeyesight/chat/config/websocket/WebSocketConfig.java
+++ src/main/java/beyondeyesight/chat/config/websocket/WebSocketConfig.java
@@ -20,7 +20,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*");
+        registry.addEndpoint("/ws-stomp")
+            .setAllowedOrigins("*")
+            .withSockJS();
     }
 
     @Override
